f44c72d736642bf1a0d9b78d5381179efaf39167
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2022-01-25 19:23:52 +0100

    init: project configuration

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.min.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+        new ESLintPlugin(),
+    ],
+};
