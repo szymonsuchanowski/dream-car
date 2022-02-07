b3b2262dd5e5c089540f0ff6795a10d6dd93a562
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2022-02-07 23:49:38 +0100

    feat: add reset style

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ResetStyle from './styled/ResetStyle';
+
+const App = () => (
+    <>
+        <ResetStyle />
+        <h1>hello</h1>
+    </>
+);
+
+export default App;
